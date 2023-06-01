4a9efca2b942989fa2b78dad302df06103c2efdb
kjyook <106497893+kjyook@users.noreply.github.com>
2023-06-01 14:19:45 +0900

    style: header 임시로  style조정

--- src/components/App/AppHeader/HeaderContent/HeaderContent.style.ts
+++ src/components/App/AppHeader/HeaderContent/HeaderContent.style.ts
@@ -2,7 +2,7 @@ import { Flex } from "@/components/UI/FlexBox";
 import styled from "@emotion/styled";
 
 export const Root = styled.div`
-  padding: 2.0rem 2.2rem 2.0rem 2.2rem;
+  padding: 2.0rem 2.2rem 2.0rem 3.2rem;
  /*  background-color: #B578B5; */
   background-color: white;
   font-size: 1.0rem;
@@ -15,4 +15,13 @@ export const Root = styled.div`
 export const Wrapper = styled(Flex)`
   width: 100%;
   height: 100%;
+
+  div{
+    width:55%;
+  }
+
+  a {
+    font-size: 1.75rem;
+    padding: 0rem 0rem 0rem 1.0rem;
+  }
 `;
